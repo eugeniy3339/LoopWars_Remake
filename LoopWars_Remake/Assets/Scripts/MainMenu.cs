deaff769daf5b6bed25b78e84e022cffce31aa8a
eugeniy3339 <122208682+eugeniy3339@users.noreply.github.com>
2026-07-06 11:07:28 +0200

    Local Multiplayer Lobby Visuals

--- LoopWars_Remake/Assets/Scripts/MainMenu.cs
+++ LoopWars_Remake/Assets/Scripts/MainMenu.cs
@@ -1,13 +1,27 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : WindowsManager
 {
+    public static MainMenu Instance { get; private set; }
+
+    [SerializeField] private GameObject lobbyPlayerUIPrefab;
+
     [Header("Local Multiplayer Lobby")]
     [SerializeField] private GameObject localMultiplayerLobbyPanel;
+    [SerializeField] private Button localMultiplayerStartButton;
+    [SerializeField] private Transform _localMultiplayerPlayersUIsContainer;
+    public Transform localMultiplayerPlayersUIsContainer { get { return _localMultiplayerPlayersUIsContainer; } }
 
     [Header("Local Multiplayer Lobby")]
     [SerializeField] private GameObject networkMultiplayerLobbyPanel;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Instance = this;
+    }
 
     public void OpenLocalMultiplayerLobby()
     {
@@ -19,5 +33,18 @@ public class MainMenu : WindowsManager
     {
         OpenPanel(networkMultiplayerLobbyPanel);
         LocalMultiplayerLobby.Instance.enabled = false;
+    }
+
+    public void ActivateLocalMultiplayerStartButton(bool activate)
+    {
+        localMultiplayerStartButton.interactable = activate;
+    }
+
+
+    public LobbyPlayerUIHandler CreateNewLobbyUI(Transform container, string name, Color color)
+    {
+        LobbyPlayerUIHandler lobbyPlayerUIHandler = Instantiate(lobbyPlayerUIPrefab, container).GetComponent<LobbyPlayerUIHandler>();
+        lobbyPlayerUIHandler.SetUp(name, false, color);
+        return lobbyPlayerUIHandler;
     }
 }
